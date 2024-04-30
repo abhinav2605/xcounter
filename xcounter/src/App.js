f667c5294eeb1b0e4f562c75894140467f5370a6
abhinav kumar <abhinav2605@yahoo.com>
2024-04-30 21:04:28 +0530

    test

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
+function App(str) {
+  const [value, setValue] = useState(0);
+  function handleChange()
+  {
+    var curr;
+    
+      curr = value + 1;
+      //setValue(value-1);
+      //curr = curr - 1;
+      //setValue(curr);
+    
+    
+  }
+  // setValue(5);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Count: {value}</p>
+      <button onClick={() => {setValue(value+1)}}>Increment</button>
+      <button onClick={() => {setValue(value-1)}}>Decrement</button>
     </div>
   );
 }
